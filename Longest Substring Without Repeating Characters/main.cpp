c8a4c90e291a01be8a4d079cc3960a5f356e5950
felixweiss1999 <felixweiss1999@gmail.com>
2023-10-01 15:24:05 +0200

    solved

--- Longest Substring Without Repeating Characters/main.cpp
+++ Longest Substring Without Repeating Characters/main.cpp
@@ -1,11 +1,25 @@
 #include <string>
+#include <unordered_map>
+
 class Solution {
 public:
-    int lengthOfLongestSubstring(std::string s) {
-        
+    static int lengthOfLongestSubstring(std::string s) {
+        std::unordered_map<char, bool> map;
+        int len = 0, max_len = 0;
+        for (char c : s){
+            if(map.count(c) > 0){ //detected duplicate!
+                max_len = std::max(len, max_len);
+                len = 0;
+                map.clear();
+            }
+            map.insert({c, true});
+            len++;
+        }
+        return std::max(len, max_len);
     }
 };
 
 int main(){
+    printf("%d", Solution::lengthOfLongestSubstring("abcab"));
     return 0;
 }
