f8956323656923a2c1722b25dca890e30ef0c774
felixweiss1999 <felixweiss1999@gmail.com>
2023-10-10 23:12:32 +0200

    solved

--- Find First and Last Position of Element in Sorted Array/main.cpp
+++ Find First and Last Position of Element in Sorted Array/main.cpp
@@ -8,28 +8,29 @@ public:
     {
         if(nums.size() == 0)
             return {-1, -1};
-        int upper = nums.size(), lower = 0;
+        int upper = nums.size()-1, lower = 0;
         int lower_bound = 0, upper_bound = nums.size();
         while (upper != lower) //first search for lower bound
         {
             int mid = (upper - lower) / 2 + lower;
-            if(nums[mid] >= target)
+            if(nums.at(mid) >= target)
                 upper = mid;
             else
                 lower = mid + 1;
         }
-        lower_bound = nums[std::max(upper, 0)] == target ? upper : -1;
-
-        upper = nums.size(), lower = 0;
+        lower_bound = nums.at(upper) == target ? upper : -1;
+        if(lower_bound == -1)
+            return {-1, -1};
+        upper = nums.size()-1, lower = 0;
         while (upper != lower)
         {
             int mid = (upper - lower + 1) / 2 + lower; //PROBLEM
-            if(nums[mid] <= target)
+            if(nums.at(mid) <= target)
                 lower = mid;
             else 
-                upper = mid - 1;
+                upper = mid-1;
         }
-        upper_bound = nums[std::min(upper, (int)nums.size()-1)] == target ? upper : -1;
+        upper_bound = nums.at(upper) == target ? upper : -1;
 
         return {lower_bound, upper_bound};
     }
@@ -38,5 +39,5 @@ public:
 int main()
 {
     std::vector<int> nums{1};
-    std::vector<int> range = Solution::searchRange(nums, 0);
+    std::vector<int> range = Solution::searchRange(nums, 1);
 }
